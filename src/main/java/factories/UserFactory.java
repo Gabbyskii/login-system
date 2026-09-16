03eae00f4fba0c1771b5d102bc49e7c3cead8abb
Gabby AK <gkadiga1@gmail.com>
2026-09-16 14:34:43 +0200

    user + userfactory done

--- src/main/java/factories/UserFactory.java
+++ src/main/java/factories/UserFactory.java
@@ -1,6 +1,39 @@
 package factories;
 
+import entities.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserFactory {
+
+    private List<User> users;
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(ArrayList<User> users) {
+        this.users = users;
+    }
+
+
+    public List<User> createUsers() {
+        users.add(new User("gabski", "1234"));
+        users.add(new User("pablo", "4321"));
+        users.add(new User("lucia", "6543"));
+        users.add(new User("jason", "1234"));
+        users.add(new User("brian", "4567"));
+        users.add(new User("cal", "9876"));
+        users.add(new User("alina", "1234"));
+        users.add(new User("lars", "6543"));;
+        users.add(new User("diana", "4321"));
+        users.add(new User("susu", "2345"));
+
+        return users;
+    }
+
+
 
 
 }
